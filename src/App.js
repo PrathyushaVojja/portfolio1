c5b557f94c35a5e88b4893b65ceab6a0d3646f8c
root <prathyusha98669688@gmail.com>
2024-01-05 02:34:12 +0000

    portfolio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Header from './components/Header/index'
+import Home from './components/Home/index'
+import Form from './components/Form/index'
+import Contact from './components/Contact/index'
+
+// Replace your code here
+const App = () => (
+  <div>
+    <Header />
+    <Home />
+    <Form />
+    <Contact />
+  </div>
+)
+
+export default App
